c27595cc814169e7824636ed1e12c15eb4dc63c5
har-shaw <8888harsha4444@gmail.com>
2023-03-11 12:13:54 -0500

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.bros.snaker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.bros.snaker to javafx.fxml;
+    exports com.bros.snaker;
+}
